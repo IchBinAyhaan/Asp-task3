e0aece6512612be236bd31123f4c74bfb2178d76
IchBinAyhaan <165468545+IchBinAyhaan@users.noreply.github.com>
2025-01-14 01:45:32 +0400

    first commit

--- Asp-task4/Asp-task4/Utilities/EmailHandler/Concrete/EmailService.cs
+++ Asp-task4/Asp-task4/Utilities/EmailHandler/Concrete/EmailService.cs
@@ -9,50 +9,44 @@ using MailKit.Net.Smtp;
 namespace Identity.Utilities.EmailHandler.Concrete
 {
     public class EmailService : IEmailService
-    {
-        private readonly EmailConfiguration _emailConfiguration;
+	{
+		private readonly EmailConfiguration _emailConfiguration;
 
-        public EmailService(EmailConfiguration emailConfiguration)
-        {
-            _emailConfiguration = emailConfiguration;
-        }
+		public EmailService(EmailConfiguration emailConfiguration)
+		{
+			_emailConfiguration = emailConfiguration;
+		}
 
-        public void SendMessage(Message message)
-        {
-            var emailMessage = CreateEmailMessage(message);
-            Send(emailMessage);
-        }
+		public void SendMessage(Message message)
+		{
+			var emailMessage = CreateEmailMessage(message);
 
-        private MimeMessage CreateEmailMessage(Message message)
-        {
-            var emailMessage = new MimeMessage();
-            emailMessage.From.Add(new MailboxAddress(_emailConfiguration.Username));
-            emailMessage.To.AddRange(message.To);
-            emailMessage.Subject = message.Subject;
-            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = message.Content };
-            return emailMessage;
-        }
+			Send(emailMessage);
+		}
 
-        private void Send(MimeMessage mailMessage)
-        {
-            using (var client = new SmtpClient())
-            {
-                try
-                {
-                    client.Connect(_emailConfiguration.SmtpServer, _emailConfiguration.Port, SecureSocketOptions.StartTls);
-                    client.Authenticate(_emailConfiguration.Username, _emailConfiguration.Password);
-                    client.Send(mailMessage);
-                }
-                catch (Exception ex)
-                {
-                    throw new Exception("An error occurred while sending the email.", ex);
-                }
-                finally
-                {
-                    client.Disconnect(true);
-                    client.Dispose();
-                }
-            }
-        }
-    }
+
+		private MimeMessage CreateEmailMessage(Message message)
+		{
+			var emailMessage = new MimeMessage();
+			emailMessage.From.Add(new MailboxAddress(_emailConfiguration.Username));
+			emailMessage.To.AddRange(message.To);
+			emailMessage.Subject = message.Subject;
+			emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = message.Content };
+			return emailMessage;
+		}
+
+		private void Send(MimeMessage message)
+		{
+			using (var client = new SmtpClient())
+			{
+
+				client.Connect(_emailConfiguration.SmtpServer, _emailConfiguration.Port, true);
+				client.AuthenticationMechanisms.Remove("XOAUTH2");
+				client.Authenticate(_emailConfiguration.Username, _emailConfiguration.Password);
+				client.Send(message);
+
+			}
+		}
+
+	}
 }
